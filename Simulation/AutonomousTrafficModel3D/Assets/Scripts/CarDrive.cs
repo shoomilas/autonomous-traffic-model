4af59e564b13faf84fa750b3834a5218e377638c
shoomilas <jakub.szumilas@gmail.com>
2021-11-15 00:06:38 +0100

    Initial forward-backward driving script and switch input method

--- Simulation/AutonomousTrafficModel3D/Assets/Scripts/CarDrive.cs
+++ Simulation/AutonomousTrafficModel3D/Assets/Scripts/CarDrive.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarDrive : MonoBehaviour
-{
+public class CarDrive : MonoBehaviour {
+    public float speed;
+    public float turnSpeed;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate() // using FixedUpdate makes it smoother for physics calculation
     {
-        
+        if (Input.GetKey(KeyCode.W)) {
+            Debug.Log("Pressed L");
+            rb.AddRelativeForce(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            rb.AddRelativeForce(-Vector3.forward * speed);
+        }
     }
 }
