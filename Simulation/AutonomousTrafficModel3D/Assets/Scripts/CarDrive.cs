d64682035a0cd184195468eed81c40025bdaa328
shoomilas <jakub.szumilas@gmail.com>
2021-11-15 01:00:34 +0100

    Edit driving

--- Simulation/AutonomousTrafficModel3D/Assets/Scripts/CarDrive.cs
+++ Simulation/AutonomousTrafficModel3D/Assets/Scripts/CarDrive.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CarDrive : MonoBehaviour {
     public float speed = 50;
     public float turnSpeed = 50;
+    public float gravityMultiplier = 50;
     private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -16,19 +17,49 @@ public class CarDrive : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate() // using FixedUpdate makes it smoother for physics calculation
     {
+        Turn();
+        Move();
+        Fall();
+        
+        // if (Input.GetKey(KeyCode.W)) {
+        //     rb.AddRelativeForce(Vector3.forward * speed);
+        // }
+        // if (Input.GetKey(KeyCode.S)) {
+        //     rb.AddRelativeForce(-Vector3.forward * speed);
+        // }
+        // if (Input.GetKey(KeyCode.D)) {
+        //     rb.AddTorque(Vector3.up * turnSpeed);
+        // }
+        // else if (Input.GetKey(KeyCode.A)) {
+        //     rb.AddTorque(- Vector3.up * turnSpeed);
+        // }
+        // rb.AddForce(Vector3.down * gravityMultiplier);
+
+    }
+
+    void Move() {
         if (Input.GetKey(KeyCode.W)) {
             rb.AddRelativeForce(Vector3.forward * speed);
         }
         if (Input.GetKey(KeyCode.S)) {
             rb.AddRelativeForce(-Vector3.forward * speed);
         }
+
+        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
+        localVelocity.x = 0;
+        rb.velocity = transform.TransformDirection(localVelocity);
+    }
+
+    void Turn() {
         if (Input.GetKey(KeyCode.D)) {
             rb.AddTorque(Vector3.up * turnSpeed);
         }
         else if (Input.GetKey(KeyCode.A)) {
             rb.AddTorque(- Vector3.up * turnSpeed);
         }
-        
-        
+    }
+
+    void Fall() {
+        rb.AddForce(Vector3.down * gravityMultiplier);
     }
 }
