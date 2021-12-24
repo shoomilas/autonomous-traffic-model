841abe3e046a2a0f1a56eda332f98a9c9eaecde1
shoomilas <jakub.szumilas@gmail.com>
2021-12-24 02:01:56 +0100

    Updates

---
+++ Simulation/AutonomousTrafficModel3D/Assets/PathCreator/Aggregator/Editor/PathAggregatorEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace PathCreator.Aggregator.Editor {
+    [CustomEditor(typeof(PathAggregator))]
+    public class PathAggregatorEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+            var pathAggregator = (PathAggregator)target;
+
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.IntField(5);
+            if (GUILayout.Button("Yo")) {
+                Debug.Log("Pressed");
+            }
+            
+            GUILayout.EndHorizontal();
+        }
+    }
+}
