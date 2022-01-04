7be76a77bec780dcb37cd03799ae5e8e28d9374b
shoomilas <jakub.szumilas@gmail.com>
2022-01-04 13:40:40 +0100

    Script for finding position on mesh

---
+++ Simulation/AutonomousTrafficModel3D/Assets/Scripts/Utils/MousePosOnMeshFromCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosOnMeshFromCamera : MonoBehaviour
+{
+    public class CarAITargetMouse : MonoBehaviour {
+
+        [SerializeField] private Transform targetTransform;
+        [SerializeField]
+        private MeshCollider meshCollider;
+        Vector3 worldPosition;
+        Ray ray;
+
+        void Start() {
+            meshCollider = meshCollider.GetComponent<MeshCollider>();
+        }
+
+        void Update()
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitData;
+
+            if(meshCollider.Raycast(ray, out hitData, 1000))
+            {
+                worldPosition = hitData.point;
+                if (targetTransform != null) {
+                    targetTransform.position = worldPosition; 
+                }
+            }
+        }
+    }
+}
