cd53d9d23ce7b2093f4d5e4d32da9f1321663fe3
shoomilas <jakub.szumilas@gmail.com>
2022-01-04 12:14:55 +0100

    Update PathNodeManager.cs

--- Simulation/AutonomousTrafficModel3D/Assets/PathCreator/Node/PathNodeManager.cs
+++ Simulation/AutonomousTrafficModel3D/Assets/PathCreator/Node/PathNodeManager.cs
@@ -1,43 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEditor;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 namespace PathCreator.Aggregator {
+    // [CreateAssetMenu(menuName = "Scriptable Objects/PathNodeManager")]
+    // public class PathNodeManager : ScriptableObject {
     public class PathNodeManager : MonoBehaviour {
-        
-        // For Naming Purposes
-        private static int currentNumber = 0;
-        public static int CurrentNumber
-        {
-            get
-            {
-                currentNumber += 1;
-                return currentNumber;
-            }
-        }
-        
         public static List<PathNode> AllThePathNodes = new List<PathNode>();
 
-        // // Draw gizmos works, handles doesn't
-        // private void OnDrawGizmos() {
-        //     foreach (var pathNode in AllThePathNodes) {
-        //         
-        //         var color = Color.red;
-        //         var radius = 1;
-        //         Gizmos.DrawWireSphere(pathNode.transform.position, radius);
-        //         
-        //         var size = 1f;
-        //         var pathNodeTransform = pathNode.transform;
-        //         Handles.color = Color.red;
-        //         Handles.ConeHandleCap(
-        //             0,
-        //             pathNodeTransform.position + new Vector3(0f, size / 2, 0f),
-        //             pathNodeTransform.rotation * Quaternion.LookRotation(Vector3.up),
-        //             size,
-        //             EventType.Repaint
-        //         );
-        //     }
-        // }
+        // private void Execute() { }
+        // private void OnValidate() { }
     }
 }
