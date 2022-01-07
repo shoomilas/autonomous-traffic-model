90937dfaf1e6400d6e51e18cee7a97a2132cc18f
shoomilas <jakub.szumilas@gmail.com>
2022-01-07 13:15:24 +0100

    Add extension methods for randomized direction choice

--- Simulation/AutonomousTrafficModel3D/Assets/PathCreator/Vehicles/VehiclePathProvider.cs
+++ Simulation/AutonomousTrafficModel3D/Assets/PathCreator/Vehicles/VehiclePathProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using PathCreator.Aggregator;
 using UnityEngine;
@@ -7,7 +8,7 @@ namespace PathCreator.Vehicles {
     public interface IVehiclePathProvider {
         List<PathNode> Provide(PathNode startNode);
     }
-    
+
     // public class VehiclePathRandomProvider : IVehiclePathProvider { }
     // public class VehiclePathNeverLeftProvider : IVehiclePathProvider { }
     public enum PathProviderMethod{
@@ -15,14 +16,27 @@ namespace PathCreator.Vehicles {
         AlwaysRight,
         AlwaysForward,
         AlwaysLeft,
-        Random,
-        RandomRightForward,
+        AlwaysRandom,
+        AlwaysRandomRightForward,
     }
-
+    
     public class VehiclePathProvider : MonoBehaviour, IVehiclePathProvider {
         private List<PathNode> finalPath = new List<PathNode>();
         public PathProviderMethod currentMethod = PathProviderMethod.FirstFound; 
 
+        public static T EnumGetRandomValue<T>() where T : Enum => 
+            Enum.GetValues(typeof(T))
+                .OfType<T>()
+                .OrderBy(_ =>Guid.NewGuid())
+                .FirstOrDefault();
+        
+        public static T EnumGetRandomValue<T>(params T[] allowedValues) where T : Enum => 
+                Enum.GetValues(typeof(T))
+                    .OfType<T>()
+                    .Where(allowedValues.Contains)
+                    .OrderBy(_ =>Guid.NewGuid())
+                    .FirstOrDefault();
+        
         public List<PathNode> Provide(PathNode startNode) {
             var path = currentMethod switch
             {
@@ -30,6 +44,7 @@ namespace PathCreator.Vehicles {
                 PathProviderMethod.AlwaysRight => GetPathNodesAlwaysInDirection(startNode, Direction.Right),
                 PathProviderMethod.AlwaysForward => GetPathNodesAlwaysInDirection(startNode, Direction.Forward),
                 PathProviderMethod.AlwaysLeft => GetPathNodesAlwaysInDirection(startNode, Direction.Left),
+                PathProviderMethod.AlwaysRandom => GetPathNodesAlwaysInDirection(startNode, EnumGetRandomValue<Direction>()),
                 _ => GetPathNodes(startNode)
             };
             return path.ToList();
